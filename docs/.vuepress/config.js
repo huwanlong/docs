2b7117865d016b8db6a86c59154770735bc9a78e
huwanlong <498107319@qq.com>
2020-11-23 22:44:32 +0800

    pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -19,6 +19,16 @@ module.exports = {
           return moment(timestamp).format("YYYY-MM-DD HH:mm:ss")
         }
       }
+    ],
+    [
+      '@vuepress/pwa',
+      {
+        serviceWorker: true,
+        updatePopup: {
+          message: "发现新内容可用",
+          buttonText: "刷新"
+        }
+      },
     ]
   ],
   themeConfig: {
