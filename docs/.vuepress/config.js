cf0f50ff17319718d5e2898eef78ade158be58b2
huwanlong <498107319@qq.com>
2020-11-24 13:05:15 +0800

    生成manifest文件的网站有问题，下载不了，所以现在icons里面的文件少了些，以后再说

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,11 +5,19 @@ module.exports = {
   title: '走在小路上',
   description: '走在小路上的笔记',
   head: [
-    ['link', { rel: 'icon', href: '/assets/img/favicon.ico' }],
     ['meta', { rel: 'author', href: '走在小路上' }],
     ['meta', { rel: 'keywords', href: '前端、Vue、Node.js、Python' }],
     ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css' }],
-    ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css' }]
+    ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css' }],
+    ['link', { rel: 'icon', href: '/icons/favicon.ico' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#0aa' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   plugins:[
     [
@@ -33,7 +41,7 @@ module.exports = {
   ],
   themeConfig: {
     lastUpdated: '更新时间',
-    logo: '/assets/img/apple-touch-icon.png',
+    logo: '/icons/apple-touch-icon.png',
     // navbar: false,
     sidebar: 'auto',
     nav: [
