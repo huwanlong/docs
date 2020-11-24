b32d9f8f3f267d9f0ea213416a6b3ce061786b40
huwanlong <498107319@qq.com>
2020-11-24 21:33:22 +0800

    返回顶部

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -41,7 +41,8 @@ module.exports = {
       clientId: '95f98d463e55a2e2a0c2',
       clientSecret: '0331c9f383b4f18d1e2a5fed26cdce59d1732447',
       autoCreateIssue: true
-    }
+    },
+    '@vuepress/back-to-top': true
   },
   themeConfig: {
     lastUpdated: '更新时间',
