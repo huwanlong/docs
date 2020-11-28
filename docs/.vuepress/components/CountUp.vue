b589d43ea3773b10c7783efc5331a8b89029254f
huwanlong <498107319@qq.com>
2020-11-28 19:58:41 +0800

    第三方组件引用以及代码引用

---
+++ docs/.vuepress/components/CountUp.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <ClientOnly>
+      <slot name="before" />
+      <span ref="countUp"></span>
+    </ClientOnly>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CountUp',
+  props: {
+    startVal: {
+      type: Number,
+      default: 0
+    },
+    endVal: {
+      type: Number,
+      required: true
+    },
+    decimalPlaces: {
+      type: Number,
+      default: 0
+    },
+    duration: {
+      type: Number,
+      default: 2
+    },
+    delay: {
+      type: Number,
+      default: 0
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  data() {
+    return {
+      counter: null
+    }
+  },
+  methods: {
+    init() {
+      import("countup.js").then(module => {
+        this.$nextTick(() => {
+          this.counter = new module.CountUp(this.$refs.countUp, this.endVal, {
+            startVal: this.startVal,
+            decimalPlaces: this.decimalPlaces,
+            duration: this.duration
+          })
+          setTimeout(() => {
+            this.counter.start()
+          }, this.delay)
+        })
+      })
+    }
+  },
+  beforeDestroy() {
+    this.counter.reset()
+    this.counter = null
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ docs/countup.md
@@ -0,0 +1,6 @@
+---
+title: CountUp 第三方组件
+---
+<CountUp :endVal="2020" />
+
+<<< @/docs/.vuepress/components/CountUp.vue
