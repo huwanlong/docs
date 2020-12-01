c3db0406e1b8f2c80c32861543563854451512b1
huwanlong <498107319@qq.com>
2020-12-01 15:06:48 +0800

    js

--- docs/.vuepress/config/sidebar.js
+++ docs/.vuepress/config/sidebar.js
@@ -1,5 +1,17 @@
 // module.exports = 'auto'
 module.exports = {
+  "/front-end/javascript/": [
+    ['', 'JavaScript介绍'],
+    '数据类型',
+    '运算符',
+    '语法',
+    '标准库',
+    '面向对象',
+    '异步操作',
+    'DOM',
+    '事件',
+    '浏览器模型'
+  ],
   "/front-end/es6/": [
     ['', 'ES6介绍'],
     'array'
---
+++ docs/front-end/javascript/README.md
@@ -0,0 +1,4 @@
+---
+title: 简介
+---
+JavaScript简介
