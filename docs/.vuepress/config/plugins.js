f5ec7e4f3399435fc78fc44df6000ec56dcaad7e
Cyan Hu <huwanlong@yeah.net>
2022-11-02 11:04:09 +0800

    feat: 因为评论功能插件中使用的代理cors-anywhere不让用了，需要自己搭一个太麻烦，所以把评论功能的配置注释掉了

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -1,40 +1,37 @@
-const secret = require('./secret')
-const moment = require('moment-timezone')
-moment.locale("zh-cn")
-moment.tz.setDefault("Asia/Shanghai")
+const secret = require("./secret");
+const moment = require("moment-timezone");
+moment.locale("zh-cn");
+moment.tz.setDefault("Asia/Shanghai");
 
 module.exports = {
-  '@vuepress/last-updated': {
+  "@vuepress/last-updated": {
     transformer: (timestamp) => {
-      return moment(timestamp).format("YYYY-MM-DD HH:mm:ss")
-    }
+      return moment(timestamp).format("YYYY-MM-DD HH:mm:ss");
+    },
   },
-  '@vuepress/pwa': {
+  "@vuepress/pwa": {
     serviceWorker: true,
     updatePopup: {
       message: "发现新内容可用",
-      buttonText: "刷新"
-    }
+      buttonText: "刷新",
+    },
   },
-  '@vssue/vuepress-plugin-vssue': {
-    // 设置 `platform` 而不是 `api`
-    platform: 'github-v4',
-    // 其他的 Vssue 配置
-    owner: 'huwanlong',
-    repo: 'docs',
-    clientId: secret.clientId,
-    clientSecret: secret.clientSecret,
-    autoCreateIssue: true
+  // '@vssue/vuepress-plugin-vssue': {
+  //   // 设置 `platform` 而不是 `api`
+  //   platform: 'github-v4',
+  //   // 其他的 Vssue 配置
+  //   owner: 'huwanlong',
+  //   repo: 'docs',
+  //   clientId: secret.clientId,
+  //   clientSecret: secret.clientSecret,
+  //   autoCreateIssue: true
+  // },
+  "@vuepress/back-to-top": true,
+  "@vuepress/google-analytics": {
+    ga: secret.ga,
   },
-  '@vuepress/back-to-top': true,
-  '@vuepress/google-analytics':
-  {
-    'ga': secret.ga
+  "@vuepress/medium-zoom": {
+    selector: "img.custom",
   },
-  '@vuepress/medium-zoom': {
-    selector: 'img.custom'
-  },
-  '@vuepress/nprogress': {
-
-  }
-}
+  "@vuepress/nprogress": {},
+};
